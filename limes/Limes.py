e14c4e8c0fe1959fab229966aa1c7f7e9f4dd83e
Václav Fröhlich <frohlich@envipartner.cz>
2022-03-24 08:18:20 +0100

    Fix problem with missing () in "Besatzung"

--- limes/Limes.py
+++ limes/Limes.py
@@ -268,7 +268,7 @@ class Limes:
         if len(array) > 0:
             result = []
             for value in array:
-                result.append('"' + attribute_name + '" ilike ' + "'%" + value + "%'")
+                result.append('("' + attribute_name + '" ilike ' + "'%" + value + "%')")
             return " OR ".join(result)
         else:
             return '"' + attribute_name + '" is not null'
